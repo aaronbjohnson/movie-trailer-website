8dd4958d9071a227e9bbc869fa7d922d4096180e
Aaron <aj65461@gmail.com>
2015-07-18 16:22:24 -0400

    Add extra info to fresh_tomatoes.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,28 +7,34 @@ youtube_key = 'AIzaSyCmJT7e_pi5-5yWNi4F9UXuV8j1fUTlIm8'
 youtube_prefix = 'https://www.youtube.com/watch?v='
 
 # Movie list -- Here you can add and subtract movies as your tastes change
-movie_list = ["There Will Be Blood", "The Life Aquatic", "Unforgiven", "Gladiator", "About Time"]
 
-# TODO: make consistent qoutes, either single or double...check style guide
+movie_list = ["There Will Be Blood", "The Life Aquatic", "Unforgiven", 
+              "Gladiator", "About Time"]
 
-# Store movie instances here
 movies = []
 
 def get_info(movie):
+    """Fetches movie info from Open Movie Database"""
+
     # Youtube stuff here
-    youtube = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + movie + 'trailer&maxResults=1&key=' + youtube_key)
+
+    youtube = requests.get('https://www.googleapis.com/youtube/v3/search?part=s'
+                           'nippet&q=' + movie + 'trailer&maxResults=1&key=' + 
+                           youtube_key)
     youtube_str = youtube.text
     youtube_dict = json.loads(youtube_str)
     video_Id = youtube_dict['items'][0]['id']['videoId']
     video_url = youtube_prefix + video_Id
 
     # Movie Database stuff here
-    result = requests.get('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json')
 
+    result = requests.get('http://www.omdbapi.com/?t=' + movie + '&y=&plot='
+                          'short&r=json')
     resp_str = result.text
 
-        # Convert data into a python dictionary
-        # http://stackoverflow.com/questions/12788217/extract-single-value-from-json-response-python
+    # Convert data into a python dictionary
+    # http://stackoverflow.com/questions/12788217/extract-single-value-from-json-response-python
+
     resp_dict = json.loads(resp_str)
 
     trailer = video_url
@@ -42,9 +48,11 @@ def get_info(movie):
     plot = resp_dict["Plot"]
     actors = resp_dict["Actors"]
 
-    movies.append(media.Movie(trailer, title, poster, release, rating, runtime, genre, director, plot, actors))
+    movies.append(media.Movie(trailer, title, poster, release, rating, runtime, 
+                              genre, director, plot, actors))
 
-# Create movie listings
+# Create movie instances and add them to the movies list
+
 for movie in movie_list:
     get_info(movie)
 
