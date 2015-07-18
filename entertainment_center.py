1442404553c40649e8399c2534a63d7fd96b93f7
Aaron <aj65461@gmail.com>
2015-07-18 15:02:27 -0400

    Create for loop that runs get_info function on all movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,7 +7,7 @@ import requests
 import json
 
 # Take this out -- only for testing
-import webbrowser
+#import webbrowser
 
 youtube_key = 'AIzaSyCmJT7e_pi5-5yWNi4F9UXuV8j1fUTlIm8'
 youtube_prefix = 'https://www.youtube.com/watch?v='
@@ -18,16 +18,17 @@ movie_list = ["There Will Be Blood", "The Life Aquatic", "Unforgiven"]
 
 # TODO: make consistent qoutes, either single or double...check style guide
 
-def getApi(movie):
+# Store movies created here
+movies = []
+
+
+def get_info(movie):
     # Youtube stuff here
     youtube = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + movie + 'trailer&maxResults=1&key=' + youtube_key)
     youtube_str = youtube.text
     youtube_dict = json.loads(youtube_str)
     video_Id = youtube_dict['items'][0]['id']['videoId']
     video_url = youtube_prefix + video_Id
-
-        # Open the trailer...just for testing
-    # webbrowser.open(video_url)
 
     # Movie Database stuff here
     result = requests.get('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json')
@@ -37,7 +38,6 @@ def getApi(movie):
         # Convert data into a python dictionary
         # http://stackoverflow.com/questions/12788217/extract-single-value-from-json-response-python
     resp_dict = json.loads(resp_str)
-    print(resp_dict)
 
     trailer = video_url
     title = resp_dict["Title"]
@@ -49,13 +49,13 @@ def getApi(movie):
     director = resp_dict["Director"]
     plot = resp_dict["Plot"]
     actors = resp_dict["Actors"]
-    print(release)
-    print(rating)
-    print(runtime)
-    print(genre)
-    print(director)
-    print(plot)
-    print(actors)
 
-getApi(movie_list[2])
+    movies.append(media.Movie(trailer, title, poster, release, rating, runtime, genre, director, plot, actors))
+
+
+# Create movie listings
+for movie in movie_list:
+    get_info(movie)
+
+fresh_tomatoes.open_movies_page(movies)
 
